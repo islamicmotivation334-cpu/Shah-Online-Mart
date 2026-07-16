9e2253dbfa40f481f00403bc1b3358ccf3456fef
islamicmotivation334-cpu <islamicmotivation334@gmail.com>
2026-07-16 15:40:04 +0600

    Create script.js

---
+++ script.js
@@ -0,0 +1,13 @@
+// Shah Online Mart
+
+document.addEventListener("DOMContentLoaded", function () {
+  console.log("Shah Online Mart Website Loaded!");
+
+  const btn = document.querySelector(".btn");
+
+  if (btn) {
+    btn.addEventListener("click", function () {
+      alert("ধন্যবাদ! আপনাকে WhatsApp-এ নিয়ে যাওয়া হচ্ছে...");
+    });
+  }
+});
